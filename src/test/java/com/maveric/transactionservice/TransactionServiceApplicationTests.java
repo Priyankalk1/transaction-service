0581ac44a8b4f95fd6e2f0a496b2b69d373e887b
Priyankalk1 <Priyankalk@maveric-systems.com>
2022-08-22 21:57:50 +0530

    Initial commit

---
+++ src/test/java/com/maveric/transactionservice/TransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maveric.transactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
